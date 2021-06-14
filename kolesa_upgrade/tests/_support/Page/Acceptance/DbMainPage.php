8ce1c6373972d3cf9af4b1a86615b5da30259d10
Sarsenali Zhunisbek <zhunisbek.s@corp.bloomzed.com>
2021-06-14 15:08:15 +0600

    Test Db

---
+++ kolesa_upgrade/tests/_support/Page/Acceptance/DbMainPage.php
@@ -0,0 +1,20 @@
+<?php
+namespace Page\Acceptance;
+
+/**
+ * Класс для главной страницы
+*/
+class DbMainPage
+{
+
+        /**
+     *  Селектр для блока пользователья
+     */
+    public static $block = ".user-card";
+    
+        /**
+     *  Селектр для кнопки Snap
+     */
+    public static $BtnSnap = "#snap";
+
+}
